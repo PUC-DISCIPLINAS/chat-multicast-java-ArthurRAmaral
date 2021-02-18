2300120a9a1693909b04260245a7fbbedad56693
Arthur Rocha Amaral <ruhtraamaral@hotmail.com>
2021-02-18 00:18:02 -0300

    Fix class name

--- src/interfaces/SerializeConverter.java
+++ src/interfaces/SerializeConverter.java
@@ -3,7 +3,7 @@ package interfaces;
 import java.io.IOException;
 import java.io.Serializable;
 
-public interface SerializeCoverter<T extends Serializable> {
+public interface SerializeConverter<T extends Serializable> {
     byte[] serialize(T obj) throws IOException;
 
     T deserialize(byte[] data) throws IOException, ClassNotFoundException;
